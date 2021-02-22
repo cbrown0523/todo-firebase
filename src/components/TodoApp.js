f2d19417df5f0dee5870f40ac3f35665d373cc37
unknown <chrisitne.brown@bhcc.edu>
2021-02-22 18:38:08 -0500

    attempting to fix functions

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -5,13 +5,51 @@ import firebase from './firebase';
     
 function TodoApp() {
   const [task, setTask] = useState("");
-  const [tasklist, setTaskList] = useState([]);
+  const [tasklist, setTaskList] = useState([]); 
+  const [idOfUpdate, setIdOfUpdate]= useState(null);
+  const [truth, setTruth] = useState();
+ 
+  const markCompleteGlobal = () => {
+    let id = idOfUpdate;
+    const itemtoupdate = firebase
+    .firestore()
+    .collection("tasks")
+    .doc(id);
+    
+    itemtoupdate.update({
+    completed: truth,
+    });
+    // debugger
+    setIdOfUpdate(null);
+    setTruth(null);
+    };
+    const markComplete = (id) => {
 
+       
+        console.log("First", idOfUpdate);
+        setIdOfUpdate(id);
+        
+        setTask(
+        task.map((tasks) => {
+        if (tasks.id === id) {
+        tasks.completed = !tasks.completed;
+        
+        setTimeout(function() {
+        setTruth(tasks.completed);
+        }, 1000);
+        }
+        return tasks;
+        })
+        )
+        console.log("Second", idOfUpdate, truth);
+        };
+
+  const AddTask=(title)=>{
   const datas={
         id: firebase.firestore()
-        .collection("task")
+        .collection("tasks")
         .doc().id
-     }
+  };
    const db=firebase.firestore();
       db.collection('tasks')
       .doc(datas.id)
@@ -19,29 +57,31 @@ function TodoApp() {
         task:task,
         id:datas.id,
         isCompleted:false}) 
-        . then(() =>{
-            console.log("display")
-        })
-    };
-        const populate = () =>{
+        .then(() =>{
+            populate();
+        });
+};
+        const populate = (data) =>{
+            setTaskList([]);
             return firebase
             .firestore()
-            .collections("task")
+            .collection("tasks")
             .get()
             .then(function(querySnapshot){
                 querySnapshot.forEach(function(doc){
                     let newData=doc.data();
-                    if(todos.indexof(newData.id)=== -1){
-                        tasklist((arr) =>{
+                    if(tasklist.indexOf(newData.id)=== -1){
+                        setTaskList((arr) =>{
                           return [...arr, newData]  
                         })
                     }else{
                         console.log("this is copy");
                     }
-                    console.log('here are all of the todos', task);
-                })
+                    console.log('here are all of the todos', tasklist);
+                });
             })
-        
+             
+        };
     
     
   const handleChange = (e) => {
@@ -52,7 +92,7 @@ function TodoApp() {
       
     
 
-  const AddTask = () => {
+  /*const AddTask = () => {
     if (task !== "") {
       const taskDetails = {
         id: Math.floor(Math.random() * 1000),
@@ -63,12 +103,26 @@ function TodoApp() {
       
         setTaskList([...tasklist, taskDetails]);
     }
+  };*/
+
+ const deletetask = (e, id) => {
+ e.preventDefault();
+  setTaskList(tasklist.filter((t) => t.id != id));
+   
   };
-  
-  const deletetask = (e, id) => {
-    e.preventDefault();
-    setTaskList(tasklist.filter((t) => t.id != id));
-  };
+  const delTodo = (id) => {
+const db = firebase.firestore();
+db.collection("tasks")
+.doc(id)
+.delete()
+/*.then(() => { setTaskList(tasklist.filter((task) => task.id != id));
+
+})*/
+.catch((error) => {
+console.error(id, "Error removing document: ", error);
+})
+.then((res) =>setTaskList([...task.filter((task) => task.id !== id)]));
+};
 
   const taskCompleted = (e, id) => {
     e.preventDefault();
@@ -88,7 +142,6 @@ function TodoApp() {
   };
 
 
-    console.log('tasklist', tasklist);
     return (
         <div className="todo">
       <input type="text" name="text" id="text" onChange = {(e)=> handleChange(e)} placeholder="Add a task here ..."/>
@@ -101,8 +154,8 @@ function TodoApp() {
             <li className={t.isCompleted ? "crossText" : "listitem"}>
               {t.value}
               <button className="completed"
-                onClick={(e) => taskCompleted(e, t.id)}>Completed </button>  
-                <button className="delete" onClick={(e) => deletetask(e, t.id)}>
+                onClick={(e) => markComplete(e, t.id)}>Completed </button>  
+                <button className="delete" onClick={(e) => delTodo(e, t.id)}>
                 Delete
               </button>
             </li>
@@ -111,6 +164,8 @@ function TodoApp() {
       ) : null}
     </div>
   );
-}
 
+}
 export default TodoApp;
+
+///onChange={props.markComplete.bind(this, props.todo.id)}
